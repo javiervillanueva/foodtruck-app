f4c4a4a10b0fc565ed56b5b53a87df6ed47847fe
Javier <javier@Julies-MacBook-Pro.local>
2019-09-24 20:57:27 -0600

    user login and signup working

--- server/controller.js
+++ server/controller.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt")
 module.exports = {
 
    createUser: async (req, res) => {
-        try {
+       try {
           const db = req.app.get("db");
       
           const hash = await bcrypt.hash(req.body.password, 10);
@@ -43,5 +43,58 @@ module.exports = {
           console.log(error);
           res.status(500).send(error);
         }
-      }
+      },
+
+      getUser: (req, res) => {
+        const db = req.app.get("db");
+        db.query(
+        `SELECT * FROM users;`
+        ) .then(results => {
+            res.send(results)
+        })
+      },
+
+      createVendor: async (req, res) => {
+        try {
+           const db = req.app.get("db");
+       
+           const hash = await bcrypt.hash(req.body.password, 10);
+       
+           const newVendor = await db.vendor.insert({
+             vendor_name: req.body.vendorName,
+             owner_name: req.body.ownerName,
+             email: req.body.email,
+             password: hash
+           });
+       
+           delete newVendor.password;
+           res.send(newVendor);
+         } catch (error) {
+           console.log(error);
+           res.status(500).send(error);
+         };
+       },
+ 
+ 
+     loginVendor: async (req, res) => {
+         try {
+           const db = req.app.get("db");
+           const [vendor] = await db.vendor.find({ email: req.body.email });
+           if (!vendor) return res.status(400).send("vendor not working");
+       
+           const authenticated = await bcrypt.compare(
+             req.body.password,
+             vendor.password
+           );
+           if (!authenticated)
+             return res.status(400).send("authenticated not working");
+       
+           delete vendor.password;
+           req.session.vendor = vendor;
+           return res.send("success yay");
+         } catch (error) {
+           console.log(error);
+           res.status(500).send(error);
+         }
+       },
 }
