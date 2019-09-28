beadd3ac8f520aad76c2a5677651d8e7f051581b
draymccomb <draymccomb@gmail.com>
2019-09-28 12:43:34 -0600

    Sign uo button functions work

--- src/Components/Vendor login/Vendorlogin.js
+++ src/Components/Vendor login/Vendorlogin.js
@@ -51,9 +51,12 @@ class Login extends React.Component {
             <div className="submit" onClick={this.handleLogin}>
               <span>Login</span>
             </div>
-            {/* <Link className="submit" to="/signup">
-              Signup
-            </Link> */}
+            <Link className="submit" to="/user/signup">
+              Signup for Foodie Account
+            </Link> 
+             <Link className="submit" to="/vendor/signup">
+              Signup for Vendor Account
+            </Link> 
           </div>
         </div>
       </div>
