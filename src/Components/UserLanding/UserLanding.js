5b3e31b36c2b1a9818b8cdeeb84e22db97d440c9
sammarston15 <sammarston15@gmail.com>
2019-10-01 20:01:08 -0600

    UserDrawer and VendorDrawer

--- src/Components/UserLanding/UserLanding.js
+++ src/Components/UserLanding/UserLanding.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './reset.css';
 import './UserLanding.css';
 import { Link } from 'react-router-dom';
-import BurgerDrawer from './BurgerDrawer';
+import UserDrawer from './UserDrawer';
 import Map from './ReactMapGL';
 
 
@@ -15,11 +15,17 @@ export default class UserLanding extends Component {
       return (
         <div className="user-landing-body">
           <div className="header">
-              <BurgerDrawer />
+              <UserDrawer />
           </div>
           <div className="map-container"><Map /></div> 
           <div className="event-list-container">
             EVENTS
+            <div className="event-summary">
+              <div className="event-profile-container">
+                <div className="event-profile"></div>
+              </div>
+              <div className="event-details">DETAILS</div>
+            </div>
             <div className="event-summary">
               <div className="event-profile-container">
                 <div className="event-profile"></div>
