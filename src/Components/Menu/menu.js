e236c8d5ff203b492179520d43f2f47c6f152820
Michael Bossart <32489711+MichaelBossart@users.noreply.github.com>
2019-10-12 10:40:05 -0600

    even more style changes

--- src/Components/Menu/menu.js
+++ src/Components/Menu/menu.js
@@ -11,13 +11,13 @@ export default class VendorMenuCard extends React.Component {
                 <div className= "topBox">
                     <div className="foodImage"></div>
                     <div className ="textBoxes">
-                        <div className ="foodBox">Taco cat's famous best Taco!</div>
-                        <div className ="moneyBox"> <h1>Much money</h1></div>
+                        <div className ="foodBox"><h1 className="foodName">Taco cat's famous best Taco!</h1></div>
+                        <div className ="moneyBox"> <h1 className="cost">10$</h1></div>
                     </div>
                 </div>
                     <div className="bottompart">
                         <div className="discription">
-                            <h1> We only make tacos with really good food bits.
+                            <h1 className="foodDiscription"> We only make tacos with really good food bits.
                                 each Taco is made WITH SO MUCH love! we cant even handle it! 
                             </h1>
                         </div>
