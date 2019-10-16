967aaabf70ea440b1f674e441e5d71781c15ee74
draymccomb <draymccomb@gmail.com>
2019-10-15 20:52:15 -0600

    Change to get events by date

--- src/Components/User-Pages/UserLanding/UserLanding.js
+++ src/Components/User-Pages/UserLanding/UserLanding.js
@@ -1,71 +1,93 @@
-import React, { Component } from 'react';
-import './reset.css';
-import './vendorDisplayCard.css';
-import './UserLanding.css';
+import React, { Component } from "react";
+import "./reset.css";
+import "./vendorDisplayCard.css";
+import "./UserLanding.css";
 // import { Link } from 'react-router-dom';
-import UserDrawer from './UserDrawer';
-import Map from './ReactMapGL';
-import {connect} from 'react-redux';
-import { logout, getAllVendors} from '../../../redux/actions'
-import axios from 'axios';
-
-
+import UserDrawer from "./UserDrawer";
+import Map from "./ReactMapGL";
+import { connect } from "react-redux";
+import { logout, getTodaysEvents } from "../../../redux/actions";
+import axios from "axios";
 
 class UserLanding extends Component {
-
-  handleLogout = (closeDrawer) => {
+  handleLogout = closeDrawer => {
     // console.log('fire log out button')
-     return axios.delete('/api/logout').then(() => {
+    return axios.delete("/api/logout").then(() => {
       this.props.logout();
-      alert('successfully logged out')
+      alert("successfully logged out");
       closeDrawer();
-      this.props.history.push('/');
+      this.props.history.push("/");
+    });
+  };
+  componentDidMount() {
+    const date = new Date();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
 
-    })
+    const todaysDate = `${year}-${month}-${day}`;
+    const body = {
+      todaysDate: todaysDate
+    };
+    console.log(body);
+    axios
+      .post("/api/get-locations-date", body)
+      .then(response => {
+        this.props.getTodaysEvents(response.data);
+        console.log(response.data);
+      })
+      .catch(console.log);
   }
-  componentDidMount(){
-    axios.get("/api/get-all-vendor")
-   .then(response =>{
-       this.props.getAllVendors(response.data);
-   });
-}
 
   render() {
-      console.log(this.props.vendors)
-      let vendorMap=this.props.vendors.map((vendor)=>{
-        return(
-            <div className="event-summary"  key={vendor.id}>
-              <div className="event-profile-container">
-                <div className="event-profile"></div>
-              </div>
-              <div className="event-details">
-              {vendor.vendor_name}
-              </div>
-            </div>
-        );
-     })
+    console.log(this.props.events);
+    let vendorMap = this.props.events.map(events => {
       return (
-        <div className="user-landing-body">
-          <div className="header">
-              <UserDrawer isUserLoggedIn={this.props.isUserLoggedIn}
-                          logout={this.handleLogout}/>
+        <div className="event-summary" key={events.vendor_id}>
+          <div className="event-profile-container">
+            <div className="event-profile"></div>
           </div>
-          <div className="map-container"><Map /></div> 
-            <h1>EVENTS</h1>
-          <div className="event-list-container">
-            {vendorMap}
+          <div className="event-details">
+            <div>{events.vendor_name}</div>
+            <div>{events.address1}</div>
+            <div>{events.address2}</div>
+            <div>
+              {events.city} {events.state}, {events.zipcode}
+            </div>
           </div>
         </div>
       );
-  };
-};
+    });
+    return (
+      <div className="user-landing-body">
+        <div className="header">
+          <UserDrawer
+            isUserLoggedIn={this.props.isUserLoggedIn}
+            logout={this.handleLogout}
+          />
+        </div>
+        <div className="map-container">
+          <Map />
+        </div>
+        <h1>EVENTS</h1>
+        <div className="event-list-container">{vendorMap}</div>
+      </div>
+    );
+  }
+}
 
 function mapStateToProps(state) {
   return {
     isUserLoggedIn: state.isUserLoggedIn,
-    vendors: state.vendors
-  }
+    vendors: state.vendors,
+    events: state.events
+  };
 }
 
-export default connect(mapStateToProps, {logout: logout, getAllVendors:getAllVendors})(UserLanding);
-
+export default connect(
+  mapStateToProps,
+  {
+    logout: logout,
+    getTodaysEvents: getTodaysEvents
+  }
+)(UserLanding);
