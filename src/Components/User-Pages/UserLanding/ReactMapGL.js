a178f3dab16737747d820a4752d2da50200baa59
sammarston15 <sammarston15@gmail.com>
2019-10-15 20:57:50 -0600

    started marker

--- src/Components/User-Pages/UserLanding/ReactMapGL.js
+++ src/Components/User-Pages/UserLanding/ReactMapGL.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, {Marker} from 'react-map-gl';
 
 export default class Map extends Component {
 state = {
@@ -37,7 +37,11 @@ onViewportChange = (viewport) => {
                     mapStyle="mapbox://styles/beardlife/ck13r7bg20gty1clg91fwjz4k"
                     onViewportChange={this.onViewportChange}
                     style={{borderRadius: '30px', margin: ' 8px 0 40px 0'}}
-                />
+                >
+                    <Marker latitude={40.3916} longitude={-111.8508} offsetLeft={-20} offsetTop={-10}>
+                        <img src="" alt="icon" />
+                    </Marker>
+                </ReactMapGL>
             </div>
         )
     }
