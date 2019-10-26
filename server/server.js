bda2f216218743984e7dbe3ff57399a2f56c22dd
sammarston15 <sammarston15@gmail.com>
2019-10-26 10:26:24 -0600

    app.set port

--- server/server.js
+++ server/server.js
@@ -62,5 +62,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build/index.html'))
 })
 
-const port = process.env.PORT || 8080
-app.listen(port, () => console.log("ready on port", port));
+app.set('port', (process.env.PORT || 8080));
+app.listen(app.get('port'), () => console.log("ready on port", app.get('port')));
