6fe13c335f11d0ca46b6aca9cead4c6ebc266870
sammarston15 <sammarston15@gmail.com>
2019-10-26 11:20:52 -0600

    fixed?

--- server/server.js
+++ server/server.js
@@ -11,8 +11,6 @@ const controller = require("./controller");
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
-server.listen(80);
-
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
