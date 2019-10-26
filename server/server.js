51cea7c08d21d1f1d1fc02b53929093362245cbd
sammarston15 <sammarston15@gmail.com>
2019-10-26 11:25:35 -0600

    ok

--- server/server.js
+++ server/server.js
@@ -3,27 +3,12 @@ const express = require("express");
 const app = express();
 const session = require("express-session");
 const massive = require("massive");
-const bcrypt = require("bcrypt");
 const bodyparser = require("body-parser");
 const path = require("path");
-const nodemailer = require("nodemailer");
 const controller = require("./controller");
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
-
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
 
 massive(process.env.CONNECTION_STRING).then(db => {
   app.set("db", db);
-});
-
-io.on("connection", function(socket) {
-  socket.emit("news", { hello: "world" });
-  socket.on("my other event", function(data) {
-    console.log(data);
-  });
 });
 
 app.use(bodyparser.json());
